b4cb5c4ba18267ec92bf5e21bda3c20d1f7f9454
ZTRdiamond <95547694+ZTRdiamond@users.noreply.github.com>
2023-09-15 19:22:40 +0700

    Update get.js

--- routes/get.js
+++ routes/get.js
@@ -2,20 +2,23 @@ module.exports = {
     name: "waifu",
     path: "/waifu",
     type: "get",
-    code: async(req, res, { axios }) => {
+    code: async (req, res, { axios }) => {
         try {
-			const response = await axios.get("https:/\/api.waifu.pics/sfw/waifu");
-			const result = JSON.stringify({
-			    status: 200,
-			    error: "none",
-			    result: [response.data]
-			}, null, 2);
-			res.setHeader("Content-Type", "application/json");
-			res.send(result);
-		} catch (e) {
-			res.status(500).json({ status: 500, error: "request to server failed" });
-			console.log("Error at waifu.js: \n" + e)
-			return;
-		}
+            const response = await axios.get("https:/\/api.waifu.pics/sfw/waifu");
+            const result = JSON.stringify({
+                status: 200,
+                error: "none",
+                result: [response.data]
+            }, null, 2);
+            res.setHeader("Content-Type", "application/json");
+            res.send(result);
+        } catch (e) {
+            res.status(500).json({
+                status: 500,
+                error: "request to server failed"
+            });
+            console.log("Error at waifu.js: \n" + e)
+            return;
+        }
     }
 }
